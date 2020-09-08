d899637546eaa17b89779528fba91d9fb624a92f
Scott McClung <scott@nerdmagik.com>
2020-09-07 18:37:09 -0700

    Add reflection of active attribute for css styling

--- force-app/main/default/lwc/link/link.js
+++ force-app/main/default/lwc/link/link.js
@@ -1,5 +1,6 @@
 import { LightningElement, api, track } from 'lwc';
-import {dispatchEvent, getRouteMatch, REGISTER_ROUTER_EVENT_NAME} from 'c/lwcRouterUtil';
+import { REGISTER_ROUTER_EVENT_NAME, dispatchEvent, getRouteMatch } from 'c/lwcRouterUtil';
+
 export default class Link extends LightningElement {
     @api label;
     @api to = '*';
@@ -9,30 +10,47 @@ export default class Link extends LightningElement {
     @track currentPath;
     @track unsubscribe;
     parentUrl
-    async connectedCallback(){
-        await getRouteMatch(this, ({path, url}) => {
+    @api
+    get active() {
+        return this._active;
+    }
+    set active(value) {
+        this._active = value;
+        this.toggleActiveAttribute();
+    }
+    async connectedCallback() {
+        await getRouteMatch(this, ({ path, url }) => {
             this.parentUrl = url;
-            if(this.to.indexOf(':url') > -1){
+            if (this.to.indexOf(':url') > -1) {
                 this.to = this.to.replace(':url', this.parentUrl);
             }
         })
         await dispatchEvent(REGISTER_ROUTER_EVENT_NAME, this, async (routerInstance) => {
             this.routerInstance = routerInstance;
             this.currentPath = this.routerInstance.currentPath;
-            this.unsubscribe = this.routerInstance.subscribe(this,this.setCurrentPath.bind(this))
+            this.unsubscribe = this.routerInstance.subscribe(this, this.setCurrentPath.bind(this))
+            this.active = this.currentPath === this.to;
         })
     }
-    setCurrentPath (){
+    setCurrentPath() {
         this.currentPath = this.routerInstance.currentPath;
     }
-    handleClick(){
-        if(this.to){
+    toggleActiveAttribute() {
+        if (this.active) {
+            this.setAttribute("active", "");
+        } else {
+            this.removeAttribute("active");
+        }
+    }
+    handleClick(e) {
+        e.stopPropagation();
+        if (this.to) {
             this.routerInstance.currentPath = this.to;
             this.currentPath = this.to;
         }
     }
-    disconnectedCallback(){
-        if(this.unsubscribe){
+    disconnectedCallback() {
+        if (this.unsubscribe) {
             this.unsubscribe.unsubscribe();
         }
     }
